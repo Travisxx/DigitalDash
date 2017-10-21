d3ad4971934899c4e14ef6e2e0a4673e6ad2b125
chenao90 <chenao90@yahoo.com>
2017-10-20 22:04:44 -0400

    Added directories properly

---
+++ js/style.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+
+init();
+
+}
+
+function init() {
+
+	var openClick = false;
+	var searchBar = document.getElementById("searchicon").addEventListener("click", addSearchBar);
+
+	function addSearchBar() {
+		
+		var parent = document.getElementById("webnav");
+		var addSearchChild = document.createElement('div');
+		addSearchChild.setAttribute("class", "pure-u-1")
+		addSearchChild.setAttribute("id", "searchbar");
+		var searchBarArray = ["<form ><input type=\"text\" name=\"email\" placeholder=\"Search...\"><input type=\"submit\" name=\"submit\"></form>"];
+		addSearchChild.innerHTML = searchBarArray;
+		parent.appendChild(addSearchChild);
+		var openClick = true;
+		removeClick(openClick);
+		
+		}
+
+	function removeClick(openClick, searchbar) {
+		if (openClick = true) {
+			searchBar = document.getElementById("searchicon").removeEventListener("click", addSearchBar);
+			openClick = false;
+		}
+
+	}	
+
+
+
+}
